0fc9143b9fa5d26a496b6c30365329d247c5698a
loncesp <loncesp@users.noreply.github.com>
2023-11-21 21:47:45 +0000

    deploy: f29a800b5f53b583b9d0fc76012584ba2fb92852

---
+++ assets/index-r4DjJVTj.js
@@ -0,0 +1,31 @@
+(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const n="/city-gpt/logo.svg";document.querySelector("#app").innerHTML=`
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${n}" class="logo"/>
+    </a>
+    <div id="message-container"></div>
+    <div class="message-input">
+      <input type="text" id="user-input" placeholder="Пожалуйста, введите ваш запрос...">
+      <button id="send-button" class="button">Отправить</button>
+    </div>
+    <script>
+  (function (w, d, s, o, f, js, fjs) {
+    w["botsonic_widget"] = o;
+    w[o] =
+      w[o] ||
+      function () {
+        (w[o].q = w[o].q || []).push(arguments);
+      };
+    (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+    js.id = o;
+    js.src = f;
+    js.async = 1;
+    fjs.parentNode.insertBefore(js, fjs);
+  })(window, document, "script", "Botsonic", "https://widget.writesonic.com/CDN/botsonic.min.js");
+  Botsonic("init", {
+    serviceBaseUrl: "https://api.botsonic.ai",
+    token: "664210e6-01e9-4149-a8ff-b9779841e546",
+  });
+<\/script>
+  </div>
+`;
