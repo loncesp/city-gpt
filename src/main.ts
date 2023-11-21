7db9bcd25bf7304ae30e3c9053d24adfa1504c32
loncesp <loncesp@gmail.com>
2023-11-22 00:49:45 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -11,24 +11,5 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <input type="text" id="user-input" placeholder="Пожалуйста, введите ваш запрос...">
       <button id="send-button" class="button">Отправить</button>
     </div>
-    <script>
-  (function (w, d, s, o, f, js, fjs) {
-    w["botsonic_widget"] = o;
-    w[o] =
-      w[o] ||
-      function () {
-        (w[o].q = w[o].q || []).push(arguments);
-      };
-    (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
-    js.id = o;
-    js.src = f;
-    js.async = 1;
-    fjs.parentNode.insertBefore(js, fjs);
-  })(window, document, "script", "Botsonic", "https://widget.writesonic.com/CDN/botsonic.min.js");
-  Botsonic("init", {
-    serviceBaseUrl: "https://api.botsonic.ai",
-    token: "664210e6-01e9-4149-a8ff-b9779841e546",
-  });
-</script>
   </div>
 `
