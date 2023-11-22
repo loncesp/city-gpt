2c316be3a2c2943bf2b2f43ffe7a473d7a40fb13
loncesp <loncesp@gmail.com>
2023-11-22 19:43:22 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,3 @@
 import './style.css'
-import logo from '/logo.svg'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <img src="${logo}" class="logo"/>
-
-    <iframe style="height:100vh;width:100vw"
-            frameBorder="0" 
-            src="https://widget.writesonic.com/CDN/index.html?service-base-url=https://api.botsonic.ai&token=664210e6-01e9-4149-a8ff-b9779841e546&base-origin=https://bot.writesonic.com&instance-name=Botsonic&standalone=true&page-url=https://bot.writesonic.com/4370752f-5062-4f22-b51f-2ba0ca96c902?t=share&workspace_id=32d2169b-bb41-4888-b4df-eaf31b381890" >
-    </iframe>
-  </div>
-`
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `<div></div>` 
